5919a663a8c092ad8390b379fce4de661f1386cf
stpn94 <stpn94@gmail.com>
2021-11-01 13:09:11 +0900

    springboot-ex #strat

---
+++ src/main/java/com/douzone/springbootex/SpringbootExApplication.java
@@ -0,0 +1,18 @@
+package com.douzone.springbootex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * Bootstrap Class
+ *
+ */
+@SpringBootApplication
+public class SpringbootExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootExApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springbootex/SpringbootExApplicationTests.java
@@ -0,0 +1,13 @@
+package springbootex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
